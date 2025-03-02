fb08ec810b6adccc8af67c43d00d20c24f0d9ca1
Chiayin <Chiayin@DESKTOP-IV3LRLM>
2025-03-02 13:32:04 -0500

    first commit

---
+++ Chiayin_Yang_MidTerm/Validate.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Chiayin_Yang_MidTerm
+{
+    partial class Validate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnAddRecord = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPalindrome = new System.Windows.Forms.Button();
+            this.txtPalindrome = new System.Windows.Forms.TextBox();
+            this.lblMatchQ2 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnValidate2 = new System.Windows.Forms.Button();
+            this.btnValidate1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnUpdateRecord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(658, 496);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 25);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Mark:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(345, 496);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 25);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 496);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 25);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "StudentID:";
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(663, 534);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(270, 31);
+            this.txtMark.TabIndex = 22;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(516, 616);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(221, 79);
+            this.btnSearch.TabIndex = 26;
+            this.btnSearch.Text = "Search Record";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(257, 616);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(221, 79);
+            this.btnDisplay.TabIndex = 25;
+            this.btnDisplay.Text = "Display Record";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnAddRecord
+            // 
+            this.btnAddRecord.Location = new System.Drawing.Point(23, 616);
+            this.btnAddRecord.Name = "btnAddRecord";
+            this.btnAddRecord.Size = new System.Drawing.Size(162, 79);
+            this.btnAddRecord.TabIndex = 24;
+            this.btnAddRecord.Text = "Add Record";
+            this.btnAddRecord.UseVisualStyleBackColor = true;
+            this.btnAddRecord.Click += new System.EventHandler(this.btnAddRecord_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(350, 534);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(270, 31);
+            this.txtName.TabIndex = 21;
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(28, 534);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(270, 31);
+            this.txtStudentID.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(984, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 25);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Palindrome:";
+            // 
+            // btnPalindrome
+            // 
+            this.btnPalindrome.Location = new System.Drawing.Point(989, 160);
+            this.btnPalindrome.Name = "btnPalindrome";
+            this.btnPalindrome.Size = new System.Drawing.Size(155, 78);
+            this.btnPalindrome.TabIndex = 19;
+            this.btnPalindrome.Text = "Palindrome";
+            this.btnPalindrome.UseVisualStyleBackColor = true;
+            this.btnPalindrome.Click += new System.EventHandler(this.btnPalindrome_Click);
+            // 
+            // txtPalindrome
+            // 
+            this.txtPalindrome.Location = new System.Drawing.Point(989, 108);
+            this.txtPalindrome.Name = "txtPalindrome";
+            this.txtPalindrome.Size = new System.Drawing.Size(284, 31);
+            this.txtPalindrome.TabIndex = 18;
+            // 
+            // lblMatchQ2
+            // 
+            this.lblMatchQ2.AutoSize = true;
+            this.lblMatchQ2.Location = new System.Drawing.Point(12, 9);
+            this.lblMatchQ2.Name = "lblMatchQ2";
+            this.lblMatchQ2.Size = new System.Drawing.Size(96, 25);
+            this.lblMatchQ2.TabIndex = 17;
+            this.lblMatchQ2.Text = "1,2,4,6,9";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(28, 90);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(343, 31);
+            this.txtInput.TabIndex = 16;
+            // 
+            // btnValidate2
+            // 
+            this.btnValidate2.Location = new System.Drawing.Point(79, 293);
+            this.btnValidate2.Name = "btnValidate2";
+            this.btnValidate2.Size = new System.Drawing.Size(240, 69);
+            this.btnValidate2.TabIndex = 15;
+            this.btnValidate2.Text = "Validate2";
+            this.btnValidate2.UseVisualStyleBackColor = true;
+            this.btnValidate2.Click += new System.EventHandler(this.btnValidate2_Click);
+            // 
+            // btnValidate1
+            // 
+            this.btnValidate1.Location = new System.Drawing.Point(79, 192);
+            this.btnValidate1.Name = "btnValidate1";
+            this.btnValidate1.Size = new System.Drawing.Size(240, 69);
+            this.btnValidate1.TabIndex = 14;
+            this.btnValidate1.Text = "Validate1";
+            this.btnValidate1.UseVisualStyleBackColor = true;
+            this.btnValidate1.Click += new System.EventHandler(this.btnValidate1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(417, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Output:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Input:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(422, 90);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(541, 370);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // btnUpdateRecord
+            // 
+            this.btnUpdateRecord.Location = new System.Drawing.Point(777, 616);
+            this.btnUpdateRecord.Name = "btnUpdateRecord";
+            this.btnUpdateRecord.Size = new System.Drawing.Size(236, 79);
+            this.btnUpdateRecord.TabIndex = 30;
+            this.btnUpdateRecord.Text = "Update Record";
+            this.btnUpdateRecord.UseVisualStyleBackColor = true;
+            this.btnUpdateRecord.Click += new System.EventHandler(this.btnUpdateRecord_Click);
+            // 
+            // Validate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 824);
+            this.Controls.Add(this.btnUpdateRecord);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMark);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnAddRecord);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnPalindrome);
+            this.Controls.Add(this.txtPalindrome);
+            this.Controls.Add(this.lblMatchQ2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnValidate2);
+            this.Controls.Add(this.btnValidate1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Validate";
+            this.Text = "Validate";
+            this.Load += new System.EventHandler(this.Validate_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnAddRecord;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPalindrome;
+        private System.Windows.Forms.TextBox txtPalindrome;
+        private System.Windows.Forms.Label lblMatchQ2;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnValidate2;
+        private System.Windows.Forms.Button btnValidate1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnUpdateRecord;
+    }
+}
+
---
+++ Chiayin_Yang_MidTerm/Validate.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chiayin_Yang_MidTerm
+{
+    public partial class Validate : Form
+    {
+        public Validate()
+        {
+            InitializeComponent();
+        }
+        private void Validate_Load(object sender, EventArgs e)
+        {
+            txtInput.Select();
+        }
+
+        // Q1
+        private void btnValidate1_Click(object sender, EventArgs e)
+        {
+            string testString = txtInput.Text;
+            string pattern1 = @"^\d{2}-\d{3}-\d{3}-\d{4}$";
+
+            if (Regex.IsMatch(testString, pattern1))
+            {
+                richTextBox1.Text = "The input matches the pattern ##-###-###-####.";
+                richTextBox1.ForeColor = Color.Green;
+                txtInput.ForeColor = Color.Green;
+            }
+            else
+            {
+                richTextBox1.Text = "The input does not match the pattern ##-###-###-####.";
+                richTextBox1.ForeColor = Color.Red;
+                txtInput.ForeColor = Color.Red;
+            }
+
+        }
+
+        private void btnValidate2_Click(object sender, EventArgs e)
+        {
+            string testString = txtInput.Text;
+            string pattern2 = @"^\d{2}\(\d{3}\)\d{3}-\d{4}$";
+
+            if (Regex.IsMatch(testString, pattern2))
+            {
+                richTextBox1.Text = "The input matches the pattern ##(###)###-####.";
+                richTextBox1.ForeColor = Color.Green;
+                txtInput.ForeColor = Color.Green;
+            }
+            else
+            {
+                richTextBox1.Text = "The input does not match the pattern ##(###)###-####.";
+                richTextBox1.ForeColor = Color.Red;
+                txtInput.ForeColor = Color.Red;
+            }
+        }
+        // Q3
+        private void btnPalindrome_Click(object sender, EventArgs e)
+        {
+            string input = txtPalindrome.Text.Trim();
+            richTextBox1.Text = "";
+
+            if (IsValidNumber(input, out string errorMessage))
+            {
+                if (IsPalindrome(input))
+                {
+                    richTextBox1.Text = "The number is a palindrome.";
+                    richTextBox1.ForeColor = Color.Green;
+                    txtPalindrome.ForeColor = Color.Green;
+                }
+                else
+                {
+                    richTextBox1.Text = "The number is not a palindrome.";
+                    richTextBox1.ForeColor = Color.Red;
+                    txtPalindrome.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                richTextBox1.Text = errorMessage;
+                richTextBox1.ForeColor = Color.Red;
+                txtPalindrome.ForeColor = Color.Red;
+            }
+        }
+        private bool IsValidNumber(string input, out string errorMessage)
+        {
+            errorMessage = "";
+
+            if (input.Length < 3 || input.Length > 9)
+            {
+                errorMessage = "The number must be between 3 and 9 digits long.";
+                return false;
+            }
+
+            if (!long.TryParse(input, out long number))
+            {
+                errorMessage = "The input must be a valid non-negative number.";
+                return false;
+            }
+
+            if (number < 0)
+            {
+                errorMessage = "The number cannot be negative.";
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsPalindrome(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedInput = new string(charArray);
+
+            return input == reversedInput;
+        }
+
+        // Q4
+        private int[] studentIDs = new int[5];
+        private string[] names = new string[5];
+        private int[] marks = new int[5];
+        private int studentCount = 0;
+
+        private void btnAddRecord_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = string.Empty;
+
+            string errorMessages = ValidateInputs();
+            if (errorMessages != string.Empty)
+            {
+                richTextBox1.Text = errorMessages;
+                return;
+            }
+
+            if (studentCount < 5)
+            {
+                int studentID = int.Parse(txtStudentID.Text);
+                string name = txtName.Text;
+                int mark = int.Parse(txtMark.Text);
+
+                studentIDs[studentCount] = studentID;
+                names[studentCount] = name;
+                marks[studentCount] = mark;
+
+                studentCount++;
+                ClearInputs();
+                richTextBox1.Text = "Student added successfully.";
+                richTextBox1.ForeColor = Color.Green;
+
+            }
+            else
+            {
+                richTextBox1.Text = "Maximum number of students reached.";
+                richTextBox1.ForeColor = Color.Red;
+            }
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            for (int i = 0; i < studentCount; i++)
+            {
+                richTextBox1.ForeColor = Color.Black;
+                richTextBox1.AppendText($"{studentIDs[i]}, {names[i]}, {marks[i]}\r\n");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = string.Empty;
+
+            if (!int.TryParse(txtStudentID.Text, out int studentID) || studentID <= 0)
+            {
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.Text = "Please enter a valid Student ID.";
+                return;
+            }
+
+            int index = Array.IndexOf(studentIDs, studentID);
+            if (index >= 0 && index < studentCount)
+            {
+                richTextBox1.ForeColor = Color.Black;
+                richTextBox1.Text = $"Student ID: {studentID}, Name: {names[index]}, Mark: {marks[index]}";
+            }
+            else
+            {
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.Text = "Student ID not found.";
+            }
+
+        }
+
+        private void btnUpdateRecord_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = string.Empty;
+
+            if (!int.TryParse(txtStudentID.Text, out int studentID) || studentID <= 0)
+            {
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.Text = "Please enter a valid Student ID.";
+                return;
+            }
+
+            if (!int.TryParse(txtMark.Text, out int newMark) || newMark < 0 || newMark > 100)
+            {
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.Text = "Please enter a valid Mark (0-100).";
+                return;
+            }
+
+            int index = Array.IndexOf(studentIDs, studentID);
+            if (index >= 0 && index < studentCount)
+            {
+                richTextBox1.ForeColor = Color.Green;
+                marks[index] = newMark;
+                richTextBox1.Text = "Mark updated successfully.";
+            }
+            else
+            {
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.Text = "Student ID not found.";
+            }
+        }
+        private string ValidateInputs()
+        {
+            string errors = string.Empty;
+
+            if (!int.TryParse(txtStudentID.Text, out int studentID) || studentID <= 0 || studentID.ToString().Length > 5 || studentIDs.Contains(studentID))
+            {
+                richTextBox1.ForeColor = Color.Red;
+                txtStudentID.BackColor = Color.Red;
+                errors += "- Invalid or duplicate Student ID (must be unique, numeric, and 1-5 digits).\r\n";
+            }
+            else
+            {
+                txtStudentID.BackColor = Color.White;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtName.Text) || txtName.Text.Length > 20)
+            {
+                richTextBox1.ForeColor = Color.Red;
+                txtName.BackColor = Color.Red;
+                errors += "- Name must be 1-20 characters and not just spaces.\r\n";
+            }
+            else
+            {
+                txtName.BackColor = Color.White;
+            }
+
+            if (!int.TryParse(txtMark.Text, out int mark) || mark < 0 || mark > 100)
+            {
+                richTextBox1.ForeColor = Color.Red;
+                txtMark.BackColor = Color.Red;
+                errors += "- Mark must be numeric and between 0 and 100.\r\n";
+            }
+            else
+            {
+                txtMark.BackColor = Color.White;
+            }
+
+            return errors;
+        }
+
+        private void ClearInputs()
+        {
+            txtStudentID.Clear();
+            txtName.Clear();
+            txtMark.Clear();
+        }
+    }
+}
